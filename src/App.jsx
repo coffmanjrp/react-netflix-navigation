3f2a70260da3ebc7fedb2640084f4582dbc394ff
coffmanjrp <coffmanjrp@gmail.com>
2021-08-06 23:32:27 -0700

    Add open/close function

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
+import { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import './App.scss';
 import logo from './img/logo.png';
 
 function App() {
+  const [visible, setVisible] = useState(false);
+
+  const handleOpen = () => setVisible(true);
+
+  const handleClose = () => setVisible(false);
+
   return (
     <div className="App">
-      <button className="nav-btn open-btn">
+      <button className="nav-btn open-btn" onClick={handleOpen}>
         <FaBars />
       </button>
       <img src={logo} alt="Logo" className="logo" />
-      <div className="nav nav-black">
-        <div className="nav nav-red">
-          <div className="nav nav-white">
-            <button className="nav-btn close-btn">
+      <div className={`nav nav-black${visible ? ' visible' : ''}`}>
+        <div className={`nav nav-red${visible ? ' visible' : ''}`}>
+          <div className={`nav nav-white${visible ? ' visible' : ''}`}>
+            <button className="nav-btn close-btn" onClick={handleClose}>
               <FaTimes />
             </button>
             <img src={logo} alt="Logo" className="logo" />
